437369250f0d9d1e3c98f680c862f184668fcc11
Luis Vela <luismvl114@gmail.com>
2022-04-18 04:20:07 -0400

    user seeder onlyDev=false

--- database/seeders/2-Users.ts
+++ database/seeders/2-Users.ts
@@ -2,8 +2,6 @@ import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from 'App/Models/User'
 
 export default class UsersSeeder extends BaseSeeder {
-  public static developmentOnly = true
-
   public async run() {
     await User.updateOrCreateMany(
       ['username', 'email'],
