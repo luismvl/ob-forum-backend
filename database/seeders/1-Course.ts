9276463555052d547ae3b90b6096ec16a31b6d30
Luis Vela <luismvl114@gmail.com>
2022-07-17 20:05:08 -0500

    change seeders now add de iconUrl to courses

--- database/seeders/1-Course.ts
+++ database/seeders/1-Course.ts
@@ -8,18 +8,28 @@ export default class CourseSeeder extends BaseSeeder {
       [
         {
           name: 'React JS',
+          iconUrl:
+            'https://res.cloudinary.com/dspkak5d0/image/upload/v1658106136/course-icons/react_kkzqxd.svg',
         },
         {
           name: 'Angular',
+          iconUrl:
+            'https://res.cloudinary.com/dspkak5d0/image/upload/v1658103541/course-icons/angular-icon_wkvm5x.svg',
         },
         {
           name: 'Spring',
+          iconUrl:
+            'https://res.cloudinary.com/dspkak5d0/image/upload/v1658106244/course-icons/spring_acy6g3.svg',
         },
         {
           name: 'JavaScript',
+          iconUrl:
+            'https://res.cloudinary.com/dspkak5d0/image/upload/v1658106244/course-icons/javascript_wnkw55.svg',
         },
         {
           name: 'Git',
+          iconUrl:
+            'https://res.cloudinary.com/dspkak5d0/image/upload/v1658106244/course-icons/git_wb3whl.svg',
         },
       ]
     )
